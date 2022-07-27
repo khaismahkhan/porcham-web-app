4aca16fe135c35a873d0768021752a2ac0ba6d2c
khaismahkhan <khaismahilyaskhan00@gmail.com>
2022-07-27 18:58:22 +0500

    initial commit

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import { Button } from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <div>
+      <Typography className="text-center text">Hello im material ui</Typography>
+      <Button>click me</Button>
+    </div>
+  );
+};
+
+export default Home;
